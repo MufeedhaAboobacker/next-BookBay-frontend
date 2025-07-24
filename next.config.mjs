c5c1ba3d2645f2728e65f0a61c400b6ffac81626
MufeedhaAboobacker <mufeedha1601@gmail.com>
2025-07-24 11:24:47 +0530

    finall

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '5000',
+        pathname: '/uploads/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'bookbay-backend-eyit.onrender.com',
+        pathname: '/uploads/**',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
